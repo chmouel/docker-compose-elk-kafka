d5f0c2545d033fb57f58b4b5682d5ee5efe90179
Chmouel Boudjnah <chmouel@chmouel.com>
2021-01-24 17:15:54 +0100

    Make it fly

--- kafka-insert-from-tfl.py
+++ kafka-insert-from-tfl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import json
 import time
 
@@ -9,5 +9,7 @@ p = Producer({"bootstrap.servers": "localhost:9092"})
 
 req = requests.get("https://api.tfl.gov.uk/line/207/arrivals/")
 for row in req.json():
+    print(row['expectedArrival'] + " - " + row['stationName'])
+
     p.produce("bus209", key=row['stationName'], value=json.dumps(row))
     time.sleep(0.1)
